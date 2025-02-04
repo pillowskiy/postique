24f11f290bef2e363ed6343911f64918c5646c58
PillowPowa <fagodfagod@gmail.com>
2025-02-04 18:42:26 +0200

    feat(storage): basic migrations

---
+++ sso/migrations/20250203161649_init.up.sql
@@ -0,0 +1,16 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+CREATE TABLE IF NOT EXISTS users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    email VARCHAR(255) UNIQUE NOT NULL,
+    pass_hash VARCHAR(255) NOT NULL,
+    is_admin BOOLEAN NOT NULL DEFAULT false
+);
+CREATE INDEX IF NOT EXISTS users_email_idx ON users(email);
+
+CREATE TABLE IF NOT EXISTS apps (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(255) UNIQUE NOT NULL,
+    secret TEXT UNIQUE NOT NULL
+);
+CREATE INDEX IF NOT EXISTS apps_name_idx ON apps(name);
