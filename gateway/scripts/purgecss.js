7074c0cee947ab666383b89db8adde87a807ac58
PillowPowa <fagodfagod@gmail.com>
2025-02-16 21:02:35 +0200

    feat(dev): nodemon, minimist for app scripts argv

--- gateway/scripts/purgecss.js
+++ gateway/scripts/purgecss.js
@@ -1,14 +1,36 @@
 import fs from 'fs';
+import minimist from 'minimist';
 import { PurgeCSS } from 'purgecss';
 
-const OUT_CSS = './public/css/postique.min.css';
-const stream = fs.createWriteStream(OUT_CSS);
+const argv = minimist(process.argv.slice(2), {
+    string: ['content', 'css', 'output'],
+});
+
+if (!argv.content) {
+    throw new Error(
+        'Content is required, please provide a content in the form of a glob',
+    );
+}
+
+if (!argv.css) {
+    throw new Error(
+        'CSS is required, please provide a css in the form of a glob',
+    );
+}
+
+if (!argv.output) {
+    throw new Error('Output is required, please provide an output file path');
+}
+
+const stream = fs.createWriteStream(argv.output);
 
 const purgeCSSResults = await new PurgeCSS().purge({
-    content: ['./views/**/*.ejs'],
-    css: ['./public/**/*.css'],
+    content: argv.content.split(','),
+    css: argv.css.split(','),
 });
 
 purgeCSSResults.forEach((result) => {
-    stream.write(result.css + '\n');
+    stream.write(
+        `/* Source: ${result.file || 'GENERATED'} */\n\n ${result.css}\n`,
+    );
 });
