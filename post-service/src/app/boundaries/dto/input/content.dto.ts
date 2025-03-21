15932fd64b39c6e4ebe17495aec217104aaaef7d
PillowPowa <fagodfagod@gmail.com>
2025-03-21 22:42:35 +0200

    feat(posts): content dtos

---
+++ post-service/src/app/boundaries/dto/input/content.dto.ts
@@ -0,0 +1,41 @@
+export class Delta {
+  constructor(
+    public readonly paragraph: Paragraph,
+    public readonly index: number,
+    public readonly type: number,
+  ) {}
+}
+
+export class Paragraph {
+  constructor(
+    public readonly name: string,
+    public readonly type: number,
+    public readonly text: string,
+    public readonly markups: Markup[],
+    public readonly metadata: ImageMetadata | undefined,
+    public readonly codeMetadata: CodeMetadata | undefined,
+  ) {}
+}
+
+export class Markup {
+  constructor(
+    public readonly type: number,
+    public readonly start: number,
+    public readonly end: number,
+  ) {}
+}
+
+export class ImageMetadata {
+  constructor(
+    public readonly id: string,
+    public readonly originalWidth: number,
+    public readonly originalHeight: number,
+  ) {}
+}
+
+export class CodeMetadata {
+  constructor(
+    public readonly lang: string,
+    public readonly spellcheck: boolean,
+  ) {}
+}
